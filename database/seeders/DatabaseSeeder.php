1e53a4fa9396ee51dc3b2a52bfedad9533def562
SOBHY <ssobhysayed@gmail.com>
2025-07-07 20:42:38 +0300

    Initial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,5 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        $this->call([
-            HealthInfoCardSeeder::class
-        ]);
     }
 }
